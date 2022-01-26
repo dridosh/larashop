21b96a79a7df54b2befeda4bdf31c14aad076ece
DmitryR <dridosh@gmail.com>
2022-01-26 20:16:19 +0400

    add routes and  npm run dev

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BasketController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +21,65 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
+})->name('main');
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::prefix('home')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/profile', [HomeController::class, 'profile'])->middleware('auth')->name('profile');
+    Route::post('/profile/update', [HomeController::class, 'profileUpdate'])->name('profileUpdate');
+});
+
+Route::prefix('basket')->group(function () {
+    Route::get('/', [BasketController::class, 'index'])->name('basket');
+    Route::post('/createOrder', [BasketController::class, 'createOrder'])->name('createOrder');
+    Route::prefix('product')->group(function () {
+        Route::post('/add', [BasketController::class, 'add'])->name('addProduct');
+        Route::post('/remove', [BasketController::class, 'remove'])->name('removeProduct');
+    });
+});
+
+Route::get('/orders', [OrderController::class, 'index'])->name('orders');
+
+
+// Route::any('/{any}', function () {
+//     return redirect(route('main'));
+// })->where('any', '.*');
+
+Auth::routes();
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/categories/{category}', [CategoryController::class, 'category'])->name('category');
+Route::get('/categories/{category}/getProducts', [CategoryController::class, 'getProducts']);
+
+Route::prefix('admin')->middleware(['auth', 'is_admin'])->group(function () {
+
+    // Route::redirect('/', '/admin/products');
+
+    /*
+    Route::get('/', function () {
+        return redirect(route('adminProducts'));
+    });
+     */
+
+    Route::get('/', [AdminController::class, 'index'])->name('admin');
+    Route::get('/enterAsUser/{userId}', [AdminController::class, 'enterAsUser'])->name('enterAsUser');
+    Route::post('/exportCategories', [AdminController::class, 'exportCategories'])->name('exportCategories');
+
+    Route::get('/products', function () {
+        return 'Админка: продукты';
+    })->name('adminProducts');
 });
